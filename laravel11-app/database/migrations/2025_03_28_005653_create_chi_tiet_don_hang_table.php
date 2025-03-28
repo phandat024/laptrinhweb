b66d7bbd78de7f6096343b62a94a7beca035e997
phandat <22211tt4678@mail.tdc.edu.vn>
2025-03-28 08:28:52 +0700

    Tao database, model, solution a va b (Chi moi lam model)

---
+++ laravel11-app/database/migrations/2025_03_28_005653_create_chi_tiet_don_hang_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chi_tiet_don_hang', function (Blueprint $table) {
+            $table->id();
+            $table->string("madonhang");
+            $table->string("masanpham")
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chi_tiet_don_hang');
+    }
+};
